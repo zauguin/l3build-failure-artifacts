b1bffb88372edb8fb0784099bc7e486cb2923f64
Marcel Fabian Krüger <tex@2krueger.de>
2022-06-20 12:57:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+import core from '@actions/core';
+import artifact from '@actions/artifact';
+import {opendir} from 'node:fs/promises';
+import {join} from 'node:path';
+
+try {
+  const name = core.getInput('name');
+  const options = {
+    retentionDays: parseInt(core.getInput('retention-days'))
+  };
+  const path = core.getInput('path');
+
+  // First find the .diff files
+  const files = [];
+  const dir = await opendir(path);
+  for await (const entry of dir) {
+    if (entry.isDirectory() && entry.name.startsWith("test")) {
+      const failures = new Set();
+      const testdirname = join(path, entry.name);
+      var testdir = await opendir(testdirname);
+      for await (const diffentry of testdir) {
+        const match = diffentry.isFile() && /^((.*)\.[^\.]*)\.diff$/.exec(diffentry.name);
+        if (match) {
+          failures.add(match[1]).add(match[2]);
+        }
+      }
+      if (failures.size > 0) {
+        testdir = await opendir(testdirname);
+        for await (const diffentry of testdir) {
+          const match = diffentry.isFile() && /^(.*)\.[^\.]*$/.exec(diffentry.name);
+          if (match && failures.has(match[1])) {
+            files.push(join(testdirname, diffentry.name));
+          }
+        }
+      }
+    }
+  }
+
+  if (files.length > 0) {
+    const artifact_client = artifact.create();
+
+    const rootDirectory = path;
+    const uploadResponse = await artifact_client.uploadArtifact(name, files, path, options)
+    if (uploadResponse.failedItems.length > 0) {
+      core.setFailed('Failed to upload some test files. The artifact archive will be incomplete!');
+    }
+  }
+} catch(err) {
+  core.setFailed(err.message);
+}
